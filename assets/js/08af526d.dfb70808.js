101ecc2780f89d52a7466a4c20784d88ce415266
Krishna <kribandi13@gmail.com>
2024-09-16 13:40:27 +0800

    Deploy website - based on 3bee32ac174882110c5c47a99daa8fb826b5d964

---
+++ assets/js/08af526d.dfb70808.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkrishna_1306_github_io=self.webpackChunkkrishna_1306_github_io||[]).push([[529],{1344:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":5,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
